26677e4885339488d67093d1f4ea93039129ecdd
Cader Hancock <Cader.Hancock@gmail.com>
2020-11-14 10:38:54 -0800

    stream refactored to method reference

--- src/main/java/bj/CardUtils.java
+++ src/main/java/bj/CardUtils.java
@@ -2,41 +2,34 @@ package bj;
 
 public class CardUtils{
 
+    public static int sumHand(Hand hand){
+        return hand.getCards().stream()
+            .mapToInt(CardUtils::gameValue)
+            .sum();
+    }
+    private static boolean handHasAce(Hand hand) {
+        return hand.getCards().contains(Card.ACE);
+    }
+    private static Integer gameValue(Card card) {
+        return switch (card){
+            case TWO -> 2;
+            case ACE -> 11;
+            case THREE -> 3;
+            case FOUR -> 4;
+            case FIVE -> 5;
+            case SIX -> 6;
+            case SEVEN -> 7;
+            case EIGHT -> 8;
+            case NINE -> 9;
+            case TEN, JACK, QUEEN, KING -> 10;
+            case CUTCARD -> {
+                yield null;
+            }
+        };
+    }
 
-	public static int sumHand(Hand hand){
+    public boolean isBust(Hand hand){
 
-		/*if(handHasAce(hand)) {
-			return 0;
-		}*/
-		return hand.getCards().stream().mapToInt(card -> gameValue(card)).sum();
-
-	}
-
-	private static boolean handHasAce(Hand hand) {
-		return hand.getCards().contains(Card.ACE);
-	}
-
-	private static Integer gameValue(Card card) {
-		 return switch (card){
-			case TWO -> 2;
-			 case ACE -> 11;
-			 case THREE -> 3;
-			 case FOUR -> 4;
-			 case FIVE -> 5;
-			 case SIX -> 6;
-			 case SEVEN -> 7;
-			 case EIGHT -> 8;
-			 case NINE -> 9;
-			 case TEN, JACK, QUEEN, KING -> 10;
-
-			 case CUTCARD -> {
-				 yield null;
-			 }
-		 };
-	}
-
-	public boolean isBust(Hand hand){
-
-	    return sumHand(hand) > 21;
+        return sumHand(hand) > 21;
     }
 }
